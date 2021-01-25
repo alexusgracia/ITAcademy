9632ce37a42e19e7d8ddeb6ce4753d40a1174e4b
Alexandre Gràcia <alexusgracia@gmail.com>
2021-01-25 12:33:53 +0100

    Exercici 2 funcions

---
+++ sprint1/3.funcions_&_template_literals/jsexercici1.2.js
@@ -0,0 +1,9 @@
+function imprimeix_nom(nom, cognom){
+    console.log(`El meu nom és ${nom} i el meu cognom ${cognom}`)
+}
+
+let nom = 'Alexandre'
+let cognom = 'Gràcia'
+imprimeix_nom(nom, cognom)
+
+
